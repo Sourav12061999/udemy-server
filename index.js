eaa84c21a584a87782bf67d4bdaf75d6407ce3c7
Sourav Das <sdunity12061999@gmail.com>
2022-01-29 20:12:08 +0530

    Again new routes added

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ require("dotenv").config(); // Importing and using dotenv
 const connect = require("./Utils/mongoose-start"); // Importing the connecting function for mongodb
 // Here I am importing all routes
 const CatagoryRoute = require("./Controllers/getCatagory");
+const CourseRoute = require("./Controllers/getCourse");
+const CourseCardRoute = require("./Controllers/getCourseCard");
+const passportRoute = require("./Controllers/passport.route");
 // Using Middlewares and app inialization
 
 const app = express(); // app initialization of express
 app.use(cors()); // Using the cors package
 
 // Using the routes
+// Routes for getting Data
 app.use("/AllCatagories", CatagoryRoute);
+app.use("/Courses", CourseRoute);
+app.use("/CourseCard", CourseCardRoute);
+app.use("/userSignup", passportRoute);
+//Route for Passport
+
 connect(); // Here connecting with mongodb
 // After the connecting has been made starting the server
 let PORT = process.env.PORT || 80;
