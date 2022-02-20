f977d88f1f600619723438e30915ccc5eb1b7a1f
Sourav Das <sdunity12061999@gmail.com>
2022-02-20 21:30:19 +0530

    ok

--- Controllers/getCourse.js
+++ Controllers/getCourse.js
@@ -8,11 +8,7 @@ const router = express.Router(); // router initialization
 // When click on a course card this will give you the main course
 router.get("/courseId=:courseID", async (req, res) => {
   try {
-    let data = await course
-      .findById(req.params.courseID)
-      .populate("comment")
-      .lean()
-      .exec();
+    let data = await course.findById(req.params.courseID).lean().exec();
     res.status(200).json(data);
   } catch (error) {
     res.status(300).json({ error: error.message });
